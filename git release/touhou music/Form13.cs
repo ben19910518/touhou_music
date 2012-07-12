b7bacfba1cdd17fc72bfc170de7b7d33724e0ca7
HalFtaN <half.a.tan@gmail.com>
2012-07-12 22:24:42 +0800

    Form4 修复，但出现了莫名其妙的问题 Signed-off-by: HalFtaN <half.a.tan@gmail.com>

---
+++ git release/touhou music/Form13.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace touhou_music
+{
+    public partial class Form13 : Form
+    {
+        public Form13()
+        {
+            InitializeComponent();
+        }
+    }
+}
